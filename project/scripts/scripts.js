0ed31fa16695f3e176eac076c2976ba3c4b2a49d
0rregol <83845391+0rregol@users.noreply.github.com>
2025-04-16 23:49:42 -0400

    Add files via upload

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -1,1 +1,11 @@
+const hamburguesa = document.getElementById('hamburger');
+const enlaces = document.querySelector('.nav-links');
+
+hamburguesa.addEventListener('click', () => {
+    enlaces.classList.toggle('show');
+});
+
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+document.getElementById('lastModified').textContent = document.lastModified;
 
